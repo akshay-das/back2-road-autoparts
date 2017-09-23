07e89603f5e7d46dc8d451dcd9d45b57047dee97
Akshay Das <akshaydaskolarikkal@gmail.com>
2017-09-23 15:14:39 -0700

    Created Gradle project and angular Webapp directory

---
+++ src/main/java/com/startup/autoparts/back2roadautoparts/Back2RoadAutopartsApplication.java
@@ -0,0 +1,12 @@
+package com.startup.autoparts.back2roadautoparts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Back2RoadAutopartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Back2RoadAutopartsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/startup/autoparts/back2roadautoparts/Back2RoadAutopartsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.startup.autoparts.back2roadautoparts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Back2RoadAutopartsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
